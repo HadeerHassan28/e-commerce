080e2e32f2e6f02c463928390e5bc0452c1b0be5
Hadeer Hassan <121343340+HadeerHassan28@users.noreply.github.com>
2023-10-11 17:17:30 +0300

    footer is done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,13 +1,46 @@
 import React from "react";
-import styles from "./Footer.module.css";
-const Footer = () => {
-  return (
-    <>
-      <div className="container">
-        <h1>Footer</h1>
-      </div>
-    </>
-  );
-};
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBCol,
+  MDBRow,
+  MDBBtn,
+  MDBInput,
+} from "mdb-react-ui-kit";
+import { Link } from "react-router-dom";
 
-export default Footer;
+export default function App() {
+  return (
+    <MDBFooter bgColor="light" className="text-center text-lg-left">
+      <MDBContainer className="p-4 pb-0">
+        <form action="">
+          <MDBRow className="d-flex justify-content-center">
+            <MDBCol size="auto" className="mb-4 mb-md-0">
+              <p className="pt-2">
+                <strong>Sign up for our newsletter</strong>
+              </p>
+            </MDBCol>
+
+            <MDBCol md="5" size="12" className="mb-4 mb-md-0">
+              <MDBInput type="text" id="form5Example2" label="Email address" />
+            </MDBCol>
+
+            <MDBCol size="auto" className="mb-4 mb-md-0 ">
+              <MDBBtn className="bg-main">Subscribe</MDBBtn>
+            </MDBCol>
+          </MDBRow>
+        </form>
+      </MDBContainer>
+
+      <div
+        className="text-center p-3"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+      >
+        &copy; {new Date().getFullYear()} Copyright:{" "}
+        <Link className="text-dark" to="">
+          FreshCart.com
+        </Link>
+      </div>
+    </MDBFooter>
+  );
+}
